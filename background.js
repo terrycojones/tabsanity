0b1d8431e4102d36201a7a4425faf696d00811e5
terrycojones <terry@fluidinfo.com>
2012-12-19 00:36:54 +0000

    Simplified / cleaned-up code.

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 var TS = {
-    tabs: {},  // key is tab id.
-    
-    sanityCheck: function(callback){
-        
+    tabs: {},  // key is tab id, values are always true.
+
+    sanityCheck: function(){
+
         chrome.tabs.query({}, function(tabs){
             // All tab ids that chrome says exists must be in this.tabs
             var idsReceived = {};
@@ -15,7 +15,7 @@ var TS = {
                 }
                 idsReceived[tabId] = true;
             }
-            
+
             // All tab ids that we currently know about must be in the results.
             for (tabId in this.tabs){
                 if (this.tabs.hasOwnProperty(tabId)){
@@ -26,120 +26,113 @@ var TS = {
                     }
                 }
             }
-            
+
         }.bind(this));
     },
 
+    mustExist: function(tabId, action){
+        if (this.tabs[tabId] === undefined){
+            console.log('Tab with id ' + tabId + ' ' + action +
+                        ', but that tab id is unknown.');
+            alert('Unknown tab ' + action + '. See console for info.');
+            return false;
+        }
+        else {
+            return true;
+        }
+    },
+
+    mustNotExist: function(tabId){
+        if (this.tabs[tabId]){
+            console.log('A tab with id ' + tabId +
+                        ' was just created, but that id is already in use.',
+                        this.tabs[tabId]);
+            alert('Duplicate tab id. See console for info.');
+            return false;
+        }
+        else {
+            return true;
+        }
+    },
+
     init: function(){
-        
+
         chrome.tabs.query({}, function(tabs){
             console.log('Found ' + tabs.length + ' pre-existing tabs.');
             for (var i = 0; i < tabs.length; i++){
-                this.tabs[tabs[i].id] = {
-                    url: tabs[i].url
-                };
+                this.tabs[tabs[i].id] = true;
             }
+            this.sanityCheck();
+            this.initListeners();
         }.bind(this));
-        
-        this.sanityCheck();
-        this.initListeners();
     },
 
     initListeners: function(){
 
+        // Add listeners for the 8 tab events given at
+        // https://developer.chrome.com/extensions/tabs.html#method-update
+
+        // 1. CREATED
         chrome.tabs.onCreated.addListener(function(tab){
             var tabId = tab.id;
             console.log('Created tag id ' + tabId);
-            if (this.tabs[tabId]){
-                console.log('A tab with id ' + tabId +
-                            ' was just created, but that id is already in use.',
-                            this.tabs[tabId]);
-                alert('Duplicate tab id. See console for info.');
-            }
-            else {
-                this.tabs[tabId] = {
-                    url: tab.url
-                };
+            if (this.mustNotExist(tabId)){
+                this.tabs[tabId] = true;
             }
             this.sanityCheck();
         }.bind(this));
 
+        // 2. UPDATED
         chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-            console.log('Updated tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' just updated, but that id has not been seen before.');
-                alert('Unknown tab updated. See console for info.');
-            }
-            else {
-                this.tabs[tabId].url = tab.url;
-            }
+            console.log('Updated (' + changeInfo.status + ') tag id ' + tabId);
+            this.mustExist(tabId, 'updated');
             this.sanityCheck();
         }.bind(this));
-        
+
+        // 3. MOVED
         chrome.tabs.onMoved.addListener(function(tabId, moveInfo){
             console.log('Moving tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' is being moved, but that id has not been seen before.');
-                alert('Unknown tab moved. See console for info.');
-            }
+            this.mustExist(tabId, 'moved');
             this.sanityCheck();
         }.bind(this));
 
+        // 4. ACTIVATED
         chrome.tabs.onActivated.addListener(function(activeInfo){
             var tabId = activeInfo.tabId;
             console.log('Activated tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' just activated, but that id has not been seen before.');
-                alert('Unknown tab activated. See console for info.');
-            }
+            this.mustExist(tabId, 'activated');
             this.sanityCheck();
         }.bind(this));
 
+        // 5. HIGHLIGHTED
         chrome.tabs.onHighlighted.addListener(function(info){
             var i, tabId;
-            for (i == 0; i < info.tabIds.length; i++){
+            for (i = 0; i < info.tabIds.length; i++){
                 tabId = info.tabIds[i];
                 console.log('Highlighted tag id ' + tabId);
-                if (this.tabs[tabId] === undefined){
-                    console.log('Tab with id ' + tabId +
-                                ' just highlighted, but that id has not been seen before.');
-                    alert('Unknown tab highlighted. See console for info.');
-                }
+                this.mustExist(tabId, 'highlighted');
             }
             this.sanityCheck();
         }.bind(this));
 
+        // 6. DETACHED
         chrome.tabs.onDetached.addListener(function(tabId, info){
             console.log('Detached tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' just detached, but that id has not been seen before.');
-                alert('Unknown tab detached. See console for info.');
-            }
+            this.mustExist(tabId, 'detached');
             this.sanityCheck();
         }.bind(this));
 
+        // 7. ATTACHED
         chrome.tabs.onAttached.addListener(function(tabId, info){
             console.log('Attached tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' just attached, but that id has not been seen before.');
-                alert('Unknown tab attached. See console for info.');
-            }
+            this.mustExist(tabId, 'attached');
             this.sanityCheck();
         }.bind(this));
-        
+
+        // 8. REMOVED
         chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
             console.log('Removing tag id ' + tabId);
-            if (this.tabs[tabId] === undefined){
-                console.log('Tab with id ' + tabId +
-                            ' is being removed, but that id has not been seen before.');
-                alert('Unknown tab removed. See console for info.');
-            }
-            else {
+            if (this.mustExist(tabId, 'removed')){
                 delete this.tabs[tabId];
             }
             this.sanityCheck();
